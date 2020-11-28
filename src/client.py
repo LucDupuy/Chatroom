ab9bc74919f2ef7dfae82418e2ecf7508734a7d5
LucDupuy <luc.dupuy@ryerson.ca>
2020-11-27 22:13:04 -0500

    Working on getting the non-blocking to work

--- src/client.py
+++ src/client.py
@@ -1,28 +1,43 @@
 import socket
 
 HOST = socket.gethostname()
-PORT = 1234
+PORT = 1127
 BUFFER_SIZE = 1024
 
 
 def client():
+    USER = input("Enter your username: ")
+
+    server_socket = setup(username=USER)
+
+
+    while True:
+        user_input = input(USER + ": ")
+        data = USER + ": " + user_input
+
+        send_data(data, server=server_socket)
+        received_data(server_socket.recv(BUFFER_SIZE))
+
+
+def setup(username):
+
     # IPV4, TCP
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.connect((HOST, PORT))
 
-    s.send(send_data())
+    data = username + ": Hello Server"
+    send_data(data, server=s)
     received_data(s.recv(BUFFER_SIZE))
 
-    s.close()
+    return s
 
 
-def send_data():
-    data = "Hello Server"
-    return data.encode()
+def send_data(data, server):
+    # Encode turns the string into bytes
+    server.send(data.encode())
 
 
 def received_data(data):
-
     print(data.decode())
 
 
--- src/server.py
+++ src/server.py
@@ -1,36 +1,49 @@
 import socket
+from pynput.keyboard import Key, Listener
 
 HOST = socket.gethostname()
-PORT = 1234
+PORT = 1127
 NUM_CONNECTIONS = 3
 BUFFER_SIZE = 1024
 
 
 def server():
-    # IPV4, TCP
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    USER = input("Enter your username: ")
 
-    s.bind((HOST, PORT))
-    s.listen(NUM_CONNECTIONS)
+    # IPV4, TCP
+
+    client, address = setup(username=USER)
+
 
     while True:
-        client, address = s.accept()
-        print(address[0], "has joined")
+        user_input = input(USER + ": ")
+        data = USER + ": " + user_input
+        send_data(data, client=client)
 
         received_data(client.recv(BUFFER_SIZE))
 
-        data = "Thank you for connecting to my server"
-        send_data(data, client=client)
+
+def setup(username):
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.bind((HOST, PORT))
+    s.listen(NUM_CONNECTIONS)
+    client, address = s.accept()
+    print(address[0], "has joined")
+    received_data(client.recv(BUFFER_SIZE))
+    data = username + ": Thank you for connecting to my server"
+    send_data(data, client=client)
+
+    return client, address
 
 
 def send_data(data, client):
     # Encode turns the string into bytes
+
     client.send(data.encode())
-    client.close()
 
 
 def received_data(data):
-    print(data)
+    print(data.decode())
 
 
 if __name__ == '__main__':
