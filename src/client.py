8f4b9b735bc642015c0f23a708799cfdd880fe12
LucDupuy <luc.dupuy@ryerson.ca>
2020-11-28 11:22:38 -0500

    Moved project to correct directory

--- src/client.py
+++ src/client.py
@@ -6,24 +6,23 @@ PORT = 1127
 BUFFER_SIZE = 1024
 
 # IPV4, TCP
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect((HOST, PORT))
+server_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_sock.connect((HOST, PORT))
 username = input("Please enter your username: ")
-
 
 
 def client():
     while True:
         try:
-            msg = client.recv(BUFFER_SIZE).decode()
+            msg = server_sock.recv(BUFFER_SIZE).decode()
             if msg == "USERNAME":
-                client.send(username.encode())
+                server_sock.send(username.encode())
             else:
                 # Seeing what the server has to say
                 print(msg)
         except:
             print("Error occurred.")
-            client.close()
+            server_sock.close()
             break
 
 
@@ -33,7 +32,7 @@ def send_data():
 
     while True:
         msg = f'{username}: {input()}'
-        client.send(msg.encode())
+        server_sock.send(msg.encode())
 
 
 read_thread = threading.Thread(target=client)
