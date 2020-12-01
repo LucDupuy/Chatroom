94c2425fbcafb5904b70ebba01276e2796baf639
LucDupuy <luc.dupuy@ryerson.ca>
2020-11-30 20:18:31 -0500

    Commented out logging comments

--- src/server.py
+++ src/server.py
@@ -52,7 +52,7 @@ def handle(client):
             client.close()
             username = usernames[client_idx]
             data = f'{username} left the chat.'
-            print(f'{username} disconnected from the server.')
+        #    print(f'{username} disconnected from the server.')
             send_data(data.encode())
             del usernames[client_idx]
             break
@@ -74,8 +74,8 @@ def server():
             continue
 
         clients.append(client)
-        print("Number of Users Connected: ", len(clients))
-        print(f"{username} has connected.")
+   #     print("Number of Users Connected: ", len(clients))
+   #     print(f"{username} has connected.")
 
         send_data(f"{username} has joined the chat.".encode())
 
