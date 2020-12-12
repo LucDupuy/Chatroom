3f88f2d08d49977d9fdc7cc0ae96e9047933b80a
LucDupuy <luc.dupuy@ryerson.ca>
2020-12-12 17:08:26 -0500

    Client side works, need to figure out how the server works

--- src/audio/client.py
+++ src/audio/client.py
@@ -1,13 +1,14 @@
 import socket
 import sys
 import threading
+import tkinter
+import tkinter.messagebox
 from threading import Thread
 import pyaudio
 import sounddevice as sd
 import numpy as np
 
-
-CHUNK = 1024
+CHUNK = 1024 * 2
 BIT_DEPTH = pyaudio.paInt16
 CHANNELS = 1
 RATE = 44100
@@ -16,3 +17,20 @@ RATE = 44100
 HOST = socket.gethostbyname("ROGUEONE")
 PORT = 80
 
+client_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+
+def client():
+    p = pyaudio.PyAudio()
+    stream = p.open(format=BIT_DEPTH, channels=CHANNELS, rate=RATE, output=True, frames_per_buffer=CHUNK)
+
+    with socket.socket() as client_socket:
+        client_socket.connect((HOST, PORT))
+        print(client_socket.recv(2048).decode('utf-8'))
+        while True:
+            data = client_socket.recv(CHUNK)
+            stream.write(data)
+
+
+if __name__ == "__main__":
+    client()
