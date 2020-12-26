5b89773534ef6e4ee68aa481e63211b9dbd98317
LucDupuy <luc.dupuy@ryerson.ca>
2020-12-26 12:59:09 -0500

    Redoing UDPm, trying to get multiple clients with just text, before we try to get voice working

--- src/voice_c.py
+++ src/voice_c.py
@@ -1,55 +1,38 @@
 import socket
-import pyaudio
 import threading
-import tkinter.messagebox
-
-# HOST = socket.gethostbyname("ilkka.ddns.net")
+# HOST = 0.0.0.0
 HOST = socket.gethostbyname("ROGUEONE")
 PORT = 80
 BUFFER_SIZE = 2048
-FORMAT = pyaudio.paInt16
-CHANNELS = 1
-RATE = 44100
+client_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-server_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-p = pyaudio.PyAudio()
-in_stream = p.open(format=FORMAT, channels=CHANNELS, rate=RATE, input=True, output=False, frames_per_buffer=BUFFER_SIZE)
-out_stream = p.open(format=FORMAT, channels=CHANNELS, rate=RATE, input=False, output=True, frames_per_buffer=BUFFER_SIZE)
-
-
-try:
-    server_sock.connect((HOST, PORT))
-except:
-    root = tkinter.Tk()
-    root.overrideredirect(1)
-    root.withdraw()
-    tkinter.messagebox.showinfo("Error", "Server is offline")
-    exit(0)
-
-
-def client():
-    while True:
-        try:
-            msg = server_sock.recv(BUFFER_SIZE)
-            # Seeing what the server has to say
-            out_stream.write(msg)
-        except socket.error:
-            server_sock.close()
-            exit(0)
 
 
 def send_data():
+
     while True:
         try:
-            mic_in = in_stream.read(BUFFER_SIZE)
-            server_sock.sendto(mic_in, (HOST, PORT))
+            msg = input()
+            client_sock.sendto(msg.encode(), (HOST, PORT))
         except socket.error as e:
-            server_sock.close()
+            client_sock.close()
             print(e)
             exit(0)
 
 
-read_thread = threading.Thread(target=client)
+
+def recv_data():
+    while True:
+
+        try:
+            msg = client_sock.recvfrom(BUFFER_SIZE)
+            if len(msg) > 0:
+                print(msg)
+        except socket.error:
+            pass
+
+
+read_thread = threading.Thread(target=recv_data)
 read_thread.start()
 
 write_thread = threading.Thread(target=send_data)
--- src/voice_s.py
+++ src/voice_s.py
@@ -1,55 +1,22 @@
 import socket
-import threading
 
 # HOST = 0.0.0.0
 HOST = socket.gethostbyname("ROGUEONE")
 PORT = 80
 BUFFER_SIZE = 2048
-
+server_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+server_sock.bind((HOST, PORT))
 
 clients = []
 
-s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-s.bind((HOST, PORT))
+while True:
+    try:
+        msg, addr = server_sock.recvfrom(BUFFER_SIZE)
+        clients.append(addr)
+        print("Connected: ", addr)
+    except socket.error:
+        pass
 
-
-
-def send_data_to_select_people(msg, idx):
     for client in clients:
-     #   if clients.index(client) != idx:
-            client.send(msg)
-
-
-def handle(client):
-    while True:
-        try:
-            data = client.recv(BUFFER_SIZE)
-            send_data_to_select_people(data, clients.index(client))
-        except socket.error:
-            client_idx = clients.index(client)
-            del clients[client_idx]
-            client.close()
-            break
-
-def server():
-    while True:
-        try:
-            client, address = s.recvfrom(BUFFER_SIZE)
-            clients.append(client)
-            thread = threading.Thread(target=handle, args=(client,))
-            thread.start()
-        except socket.error as e:
-            print("Receiving: ", e)
-            pass
-
-
-
-
-
-
-if __name__ == '__main__':
-    server()
-
-# TODO: Currently this is TCP, not UDP
-# TODO: Deal with multiple clients
-# TODO: Server should not have mic in, it should only receive and send
+      #  if client != addr:
+      server_sock.sendto(msg, addr)
