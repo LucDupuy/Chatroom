1365b76758d62480c25df042a0f808c6350153d5
LucDupuy <luc.dupuy@ryerson.ca>
2020-12-27 16:29:47 -0500

    Voice chat seems to work, at least with one client and with the server playing the audio. Want to have the client play audio? Multiple clients?

--- src/voice_c.py
+++ src/voice_c.py
@@ -4,11 +4,11 @@ from threading import Thread
 
 HOST = socket.gethostbyname("ROGUEONE")
 PORT = 80
-BUFFER_SIZE = 2048
-FORMAT = pyaudio.paInt16
-CHANNELS = 1
-RATE = 44100
 
+BUFFER_SIZE = 1024
+FORMAT = pyaudio.paInt16
+CHANNELS = 2
+RATE = 44100
 frames = []
 
 
@@ -19,22 +19,19 @@ def udp():
         if len(frames) > 0:
             sock.sendto(frames.pop(0), (HOST, PORT))
 
-
-def record(stream, CHUNK):
+def record_voice(stream, CHUNK):
     while True:
         frames.append(stream.read(CHUNK))
 
 
 if __name__ == "__main__":
     p = pyaudio.PyAudio()
-    in_stream = p.open(format=FORMAT, channels=CHANNELS, rate=RATE, input=True, output=False, frames_per_buffer=BUFFER_SIZE)
+    in_stream = p.open(format=FORMAT, channels=CHANNELS, rate=RATE, input=True, frames_per_buffer=BUFFER_SIZE)
 
-    rec_thread = Thread(target=record, args=(in_stream, BUFFER_SIZE, ))
-    rec_thread.setDaemon(True)
+    rec_thread = Thread(target=record_voice, args=(in_stream, BUFFER_SIZE,))
+    socket_thread = Thread(target=udp)
+
     rec_thread.start()
+    socket_thread.start()
     rec_thread.join()
-
-    udp_thread = Thread(target=udp)
-    udp_thread.setDaemon(True)
-    udp_thread.start()
-    udp_thread.join()
+    socket_thread.join()
--- src/voice_s.py
+++ src/voice_s.py
@@ -5,42 +5,40 @@ from threading import Thread
 HOST = '0.0.0.0'
 # HOST = socket.gethostbyname("ROGUEONE")
 PORT = 80
-BUFFER_SIZE = 2048
+
 FORMAT = pyaudio.paInt16
-CHANNELS = 1
+BUFFER_SIZE = 1024
+CHANNELS = 2
 RATE = 44100
 
 frames = []
 
 
-def udp(CHUNK):
-    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    sock.bind((HOST, PORT))
+def udpStream(CHUNK):
+    udp = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    udp.bind((HOST, PORT))
 
     while True:
-        msg, addr = sock.recvfrom(CHUNK)
-        frames.append(msg)
+        soundData, addr = udp.recvfrom(CHUNK * CHANNELS * 2)
+        frames.append(soundData)
 
 
-def play_voice(stream, CHUNK):
+def play(stream, CHUNK):
     BUFFER = 10
     while True:
         if len(frames) == BUFFER:
             while True:
-                print(frames.pop(0))
-#                stream.write(frames.pop(0), CHUNK)
+                stream.write(frames.pop(0), CHUNK)
 
 
 if __name__ == "__main__":
     p = pyaudio.PyAudio()
-    out_stream = p.open(format=FORMAT, channels=CHANNELS, rate=RATE, input=False, output=True, frames_per_buffer=BUFFER_SIZE)
+    out_stream = p.open(format=FORMAT, channels=CHANNELS, rate=RATE, output=True, frames_per_buffer=BUFFER_SIZE)
 
-    udp_thread = Thread(target=udp, args=(BUFFER_SIZE,))
-    udp_thread.setDaemon(True)
-    udp_thread.start()
-    udp_thread.join()
-
-    play_thread = Thread(target=play_voice, args=(out_stream, BUFFER_SIZE,))
-    play_thread.setDaemon(True)
-    play_thread.start()
-    play_thread.join()
+    socket_thread = Thread(target=udpStream, args=(BUFFER_SIZE,))
+    sending_thread = Thread(target=play, args=(out_stream, BUFFER_SIZE,))
+    
+    socket_thread.start()
+    sending_thread.start()
+    socket_thread.join()
+    sending_thread.join()
